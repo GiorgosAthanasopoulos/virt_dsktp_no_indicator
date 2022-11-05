d0b8a5ec5b6ae85cc21ebbeabbdbb89d9aa34c8e
Giorgos Athanasopoulos <90207655+GiorgosAthanasopoulos@users.noreply.github.com>
2022-11-06 01:39:22 +0200

    Add files via upload

---
+++ virt_dsktp_no_indicator.py
@@ -0,0 +1,25 @@
+import pystray
+from PIL import Image
+import pyvda
+import threading
+import time
+
+max_virt_dsktps = 10
+images_directory = './images/'
+
+icon = pystray.Icon('virt dsktp no indicator',icon=Image.open(images_directory + '1.png'),menu=pystray.Menu(pystray.MenuItem('Exit',lambda icon:icon.stop())))
+
+def bgThread():
+    global icon
+    global max_virt_dsktps
+
+    while True:
+        current_desktop = pyvda.VirtualDesktop.current().number
+
+        if str(current_desktop) not in icon.icon.filename and current_desktop <= max_virt_dsktps:
+            icon.icon = Image.open(images_directory + str(current_desktop) + '.png')
+
+        time.sleep(.2)
+
+threading.Thread(target=bgThread).start()
+icon.run()
